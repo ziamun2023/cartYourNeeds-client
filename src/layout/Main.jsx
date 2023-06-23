2bd58ec5604d47e760f925ed4b56a1b9c0cb6e89
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-06-23 11:26:01 +0600

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer";
+import Navbar from "../shared/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet></Outlet>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/home/Home";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:'/',
+            element:<Home/>
+        }
+      ]
+    },
+  ]);
